58c0d6d58b42ec213e15f32fbd1a9490a843118b
Ivan Welsh <ivan@welsh.co.nz>
2018-02-17 07:58:23 +1300

    Initial Commit

---
+++ SuDoKuSolver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SuDoKuSolver
+//
+//  Created by Ivan Welsh on 17/02/18.
+//  Copyright © 2018 Hermes Productions. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
