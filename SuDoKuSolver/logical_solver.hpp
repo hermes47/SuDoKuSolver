797b8dff537251b41677c198a9b52db707108889
Ivan Welsh <ivan@welsh.co.nz>
2018-02-18 11:47:29 +1300

    Added FindNakedNuples which removes options when a set of cells of size N has a combined total of N possible options.
    Cleaned up (kinda) the logging stuff.

--- SuDoKuSolver/logical_solver.hpp
+++ SuDoKuSolver/logical_solver.hpp
@@ -9,10 +9,13 @@
 #ifndef SUDOKUSOLVER_LOGICAL_SOLVER_HPP
 #define SUDOKUSOLVER_LOGICAL_SOLVER_HPP
 
+#include <iterator>
 #include <map>
+#include <sstream>
 #include <tuple>
 #include <vector>
 
+#include "combinations.hpp"
 #include "grid.hpp"
 
 enum Action {
@@ -56,6 +59,17 @@ values_t DetermineValue(std::bitset<num_vals>& options) {
   return val;
 }
 
+template <dimension_t Height, dimension_t Width, typename iterator_t>
+std::bitset<Height * Width> GetCombinedOptions(iterator_t begin, iterator_t end, SudokuGrid<Height,Width>& grid) {
+  std::bitset<Height * Width> o;
+  for (; begin != end; ++begin) {
+    std::bitset<Height * Width> tmp;
+    grid.GetCell(*begin).GetPossibleOptions(tmp);
+    o |= tmp;
+  }
+  return o;
+}
+
 template <dimension_t Height, dimension_t Width>
 void DetermineAffectedCells(work_t i, values_t val, SudokuGrid<Height,Width>& g, Actions *actions) {
   static const values_t num_vals = Height * Width;
@@ -72,6 +86,7 @@ template <dimension_t Height, dimension_t Width>
 bool FindNakedSingles(SudokuGrid<Height,Width>& grid, Actions *actions) {
   static const values_t num_vals = Height * Width;
   static const work_t num_cells = num_vals * num_vals;
+  std::stringstream ss;
   
   bool added = false;
   for (work_t idx = 0; idx < num_cells; ++idx) {
@@ -80,13 +95,14 @@ bool FindNakedSingles(SudokuGrid<Height,Width>& grid, Actions *actions) {
       std::bitset<num_vals> options;
       cell.GetPossibleOptions(options);
       values_t val = DetermineValue<num_vals>(options);
-      std::cout << "[ONLY ONE] r" << cell.GetRow() << "c" << cell.GetColumn()
-                << " must be " << val << std::endl;
+      ss << "[ONLY ONE] r" << cell.GetRow() << "c" << cell.GetColumn();
+      ss << " must be " << val << std::endl;
       DetermineAffectedCells(idx, val, grid, actions);
       actions->emplace_back(SET_VALUE, val, idx, current_group);
       added = true;
     }
   }
+  if (added) std::cout << ss.str();
   return added;
 }
 
@@ -96,6 +112,7 @@ bool FindHiddenSingles(SudokuGrid<Height,Width>& grid, Actions *actions) {
   static const work_t num_cells = num_vals * num_vals;
   static std::array<std::vector<values_t>, 3 * num_vals> grp_idxs;
   static bool init_idxs = false;
+  std::stringstream ss;
   
   if (!init_idxs) {
     for (work_t cell_idx = 0; cell_idx < num_cells; ++cell_idx) {
@@ -148,25 +165,116 @@ bool FindHiddenSingles(SudokuGrid<Height,Width>& grid, Actions *actions) {
     actions->emplace_back(SET_VALUE, kv->second.first, kv->first, current_group);
     values_t row, col, blk;
     grid.GetCellGroups(kv->first, row, col, blk);
-    std::cout << "[UNIQUE] r" << row << "c" << col << " must be " << kv->second.first
-    << " as it is the only such instance in its";
+    ss << "[UNIQUE] r" << row << "c" << col << " must be " << kv->second.first;
+    ss << " as it is the only such instance in its";
     bool addand = false;
     if (kv->second.second & 1) {
       addand = true;
-      std::cout << " Row";
+      ss << " Row";
     }
     if (kv->second.second & 2) {
-      if (addand) std::cout << " and";
+      if (addand) ss << " and";
       addand = true;
-      std::cout << " Column";
+      ss << " Column";
     }
     if (kv->second.second & 4) {
-      if (addand) std::cout << " and";
-      std::cout << " Block";
+      if (addand) ss << " and";
+      ss << " Block";
     }
-    std::cout << "." << std::endl;
+    ss << "." << std::endl;
+  }
+  if (added) std::cout << ss.str();
+  return added;
+}
+
+template <dimension_t Height, dimension_t Width>
+bool FindNakedNuples(SudokuGrid<Height,Width>& grid, Actions *actions) {
+  static const values_t num_vals = Height * Width;
+  static const work_t num_cells = num_vals * num_vals;
+  static const std::map<values_t, std::string> nuple_names {
+    { 2, "PAIR" },
+    { 3, "TRIPLE" },
+    { 4, "QUAD" },
+    { 5, "QUINT" },
+    { 6, "HEXTUPLE" },
+    { 7, "HEPTUPLE" },
+    { 8, "OCTUPLE" },
+    { 9, "NONUPLE" },
+    {10, "DECUPLE" },
+  };
+  static std::array<std::vector<values_t>, 3 * num_vals> grp_idxs;
+  static bool init_idxs = false;
+  std::stringstream ss;
+  
+  if (!init_idxs) {
+    for (work_t cell_idx = 0; cell_idx < num_cells; ++cell_idx) {
+      SudokuCell<num_vals> &cell = grid.GetCell(cell_idx);
+      grp_idxs[cell.GetRow()].push_back(cell_idx);
+      grp_idxs[cell.GetColumn() + num_vals].push_back(cell_idx);
+      grp_idxs[cell.GetBlock() + 2 * num_vals].push_back(cell_idx);
+    }
+    init_idxs = true;
   }
   
+  bool added = false;
+  // Begin with pairs and move up to num_vals / 2, but only if previous n-uple fails
+  for (values_t nuple = 2; nuple <= num_vals / 2; ++nuple) {
+    if (added) break;
+    for (values_t idx = 0; idx < grp_idxs.size(); ++idx) {
+      std::stringstream header;
+      header << "[NAKED ";
+      if (idx >= 2 * num_vals) header << "BLOCK ";
+      else if (idx >= 1 * num_vals) header << "COLUMN ";
+      else header << "ROW ";
+      if (nuple_names.find(nuple) != nuple_names.end()) header << nuple_names.at(nuple);
+      else header << nuple << "-uple";
+      header << "] ";
+      
+      std::vector<values_t> &grp = grp_idxs[idx];
+      std::vector<values_t> valids;
+      for (values_t i : grp) {
+        if (!grid.GetCell(i).GetValue()) valids.push_back(i);
+      }
+      auto combinations = MakeCombinations(valids.begin(), valids.end(), nuple);
+      std::vector<values_t> combo, missing_combo;
+      while (combinations(std::back_inserter(combo), std::back_inserter(missing_combo))) {
+        std::bitset<num_vals> combo_options, negative_options;
+        combo_options = GetCombinedOptions(combo.begin(), combo.end(), grid);
+        if (combo_options.count() == nuple) {
+          negative_options = GetCombinedOptions(missing_combo.begin(), missing_combo.end(), grid);
+          std::bitset<num_vals> intersection = combo_options & negative_options;
+          if (intersection.any()) {
+            std::stringstream naked;
+            for (values_t i : combo) {
+              SudokuCell<num_vals> &cell = grid.GetCell(i);
+              if (i != combo[0]) naked << ",";
+              naked << "r" << cell.GetRow() << "c" << cell.GetColumn();
+            }
+            for (values_t i : missing_combo) {
+              SudokuCell<num_vals> &cell = grid.GetCell(i);
+              cell.GetPossibleOptions(intersection);
+              intersection &= combo_options;
+              if (!intersection.any()) continue;
+              bool first_remove = false;
+              for (values_t val = 0; val < intersection.size(); ++val) {
+                if (!intersection[val]) continue;
+                added = true;
+                actions->emplace_back(CLEAR_VALUE, val + 1, i, current_group);
+                if (!first_remove) {
+                  ss << header.str() << naked.str() << " removes " << val + 1;
+                  first_remove = true;
+                } else ss << "," << val + 1;
+              }
+              ss << " from r" << cell.GetRow() << "c" << cell.GetColumn() << "." << std::endl;
+            }
+          }
+        }
+        combo.clear();
+        missing_combo.clear();
+      }
+    }
+  }
+  if (added) std::cout << ss.str();
   return added;
 }
 
@@ -194,7 +302,7 @@ bool LogicallySolveGrid(SudokuGrid<Height,Width>& grid) {
   
   while (true) {
     ++current_group;
-    std::cout << "Solver round " << current_group << std::endl;
+    std::cout << "Logical solver round " << current_group << std::endl;
     // Handle all actions from last round
     for (; current_action < actions.size(); ++current_action)
       HandleAction<Height, Width>(actions[current_action], grid);
@@ -206,6 +314,9 @@ bool LogicallySolveGrid(SudokuGrid<Height,Width>& grid) {
     // Check for single options
     if (FindNakedSingles(grid, &actions)) continue;
     if (FindHiddenSingles(grid, &actions)) continue;
+    
+    // Check for Naked n-uples
+    if (FindNakedNuples(grid, &actions)) continue;
     
     
     // If we reach the end of all logical methods, then cannot currently logically solve
